98052992546162c083618b0c015c0fb853f07d99
边雷 <bianlei@csii.com.cn>
2017-06-14 19:36:39 +0800

    创建工程

---
+++ ApplePayDemo/ApplePayDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ApplePayDemo
+//
+//  Created by 边雷 on 2017/6/14.
+//  Copyright © 2017年 Mac-b. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ApplePayDemo/ApplePayDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ApplePayDemo
+//
+//  Created by 边雷 on 2017/6/14.
+//  Copyright © 2017年 Mac-b. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
